8a017369b6fbf8f1079529b695300841f2b16d3f
PhamHuuViet <phamhuuviet2410@gmail.com>
2024-09-09 01:31:43 +0700

    fisrt add

---
+++ music-list.js
@@ -0,0 +1,35 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "Lofi Chill 1",
+    artist: "Hạ Sang",
+    img: "lofi-1",
+    src: "lofi-1"
+  },
+  {
+    name: "Những Ca Khúc Hay Nhất Của Andiez, Jaykii,..",
+    artist: "Andiez, Jaykii,...",
+    img: "lofi-2",
+    src: "lofi-2"
+  },
+  {
+    name: "Những Ca Khúc Hay Nhất Của JUSTATEE",
+    artist: "Justatee",
+    img: "justatee",
+    src: "justatee"
+  },
+  {
+    name: "Những Ca Khúc Hay Nhất Của RHYMASTIC",
+    artist: "Rhymastic",
+    img: "rhymastic",
+    src: "rhymastic"
+  },
+];
